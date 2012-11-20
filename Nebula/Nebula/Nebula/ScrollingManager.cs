5636ad0dbf14d50e28f32fa20c1a4cb8eb378c1b
Schuyler G <schuylergoodwin@gmail.com>
2012-11-19 19:55:16 -0500

    scrolling update

--- Nebula/Nebula/Nebula/ScrollingManager.cs
+++ Nebula/Nebula/Nebula/ScrollingManager.cs
@@ -25,9 +25,9 @@ namespace Nebula
         private float CameraSize;
         private float cameraPos = 0f;
         private double LEFT_INTERVAL;
-        private double leftMargin; 
+       // private double leftMargin; 
         private double RIGHT_INTERVAL;
-        private float rightMargin; 
+       // private float rightMargin; 
         private float MAX_CAMERA_POS; 
         private Vector2 scrollingDirection = new Vector2(-1, 0);
         private Vector2 aSpeed = new Vector2(5, 0); 
@@ -84,34 +84,34 @@ namespace Nebula
             myBackgrounds = backgroundSprites.ToList();
         }
 
-        public void ScrollBackward()
-        {
-            //Setting up so if the image that goes off the screen (player has 'passed' it)
-            //it will be added back on to the end of the images.
-            BackgroundSprite[] backgroundSprites = myBackgrounds.ToArray();
-            //for (int i = backgroundSprites.Length-1; i >= 0; i--)
-            for (int i = 0; i < backgroundSprites.Length; i++)
-            {
-                BackgroundSprite b = backgroundSprites[i];
-                if (b.myPosition.X > CameraSize + b.size.Width)
-                {
-                    //if it is the first image in the array, 
-                    // it should be added on the last image in the array
-                    if (i == backgroundSprites.Length-1)
-                    {
-                        b.myPosition.X = backgroundSprites[0].myPosition.X
-                            + backgroundSprites[0].size.Width;
-                    }
-                    else
-                    {
-                        b.myPosition.X = backgroundSprites[i + 1].myPosition.X + backgroundSprites[i + 1].size.Width;
-                    }
-                }
-            }
+        //public void ScrollBackward()
+        //{
+        //    //Setting up so if the image that goes off the screen (player has 'passed' it)
+        //    //it will be added back on to the end of the images.
+        //    BackgroundSprite[] backgroundSprites = myBackgrounds.ToArray();
+        //    //for (int i = backgroundSprites.Length-1; i >= 0; i--)
+        //    for (int i = 0; i < backgroundSprites.Length; i++)
+        //    {
+        //        BackgroundSprite b = backgroundSprites[i];
+        //        if (b.myPosition.X > CameraSize + b.size.Width)
+        //        {
+        //            //if it is the first image in the array, 
+        //            // it should be added on the last image in the array
+        //            if (i == backgroundSprites.Length-1)
+        //            {
+        //                b.myPosition.X = backgroundSprites[0].myPosition.X
+        //                    + backgroundSprites[0].size.Width;
+        //            }
+        //            else
+        //            {
+        //                b.myPosition.X = backgroundSprites[i + 1].myPosition.X + backgroundSprites[i + 1].size.Width;
+        //            }
+        //        }
+        //    }
 
-            //putting the result of these positions back into the list holding all the sprites 
-            myBackgrounds = backgroundSprites.ToList();
-        } 
+        //    //putting the result of these positions back into the list holding all the sprites 
+        //    myBackgrounds = backgroundSprites.ToList();
+        //} 
 
         public void Update(double totalSecs)
         { 
@@ -121,7 +121,8 @@ namespace Nebula
             if (charPos < LEFT_INTERVAL)
             {
                 //cameraPos = charPos - (float)LEFT_INTERVAL;
-                ScrollBackward(); 
+                //ScrollBackward(); 
+                ScrollForward();
                 foreach (BackgroundSprite bs in myBackgrounds)
                 {
                     //bs.myPosition += -scrollingDirection * aSpeed * (float)totalSecs;
@@ -136,7 +137,7 @@ namespace Nebula
                 {
                     //bs.myPosition += scrollingDirection * aSpeed * (float)totalSecs; 
                     //bs.myPosition += new Vector2(cameraMovement, 0)*scrollingDirection*aSpeed; 
-                    bs.myPosition += scrollingDirection * aSpeed;
+                   bs.myPosition += scrollingDirection * aSpeed;
                 }
             }
 
@@ -151,6 +152,8 @@ namespace Nebula
 
         public void Draw(SpriteBatch batch)
         {
+
+
             //draws all the background sprites
             foreach (Sprite b in myBackgrounds)
             {
